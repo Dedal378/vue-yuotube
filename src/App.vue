035fba8c2112a1c19a4fc3d7a05b5c296c67823b
Dedal378 <orlovetsfoto@gmail.com>
2022-02-21 11:52:29 +0300

    mob sidebar

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <header class="flex justify-between fixed z-30 w-full">
     <div class="lg:w-1/4 flex">
       <div class="flex items-center xl:w-64 xl:bg-white pl-4">
-        <button class="mr-3 sm:ml-2 sm:mr- 6">
+        <button class="mr-3 sm:ml-2 sm:mr-6">
           <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M4 6h16M4 12h16M4 18h16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
           </svg>
