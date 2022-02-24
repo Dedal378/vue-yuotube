88d477af60bda3d3e896247abb42f0255893ca6d
Dedal378 <orlovetsfoto@gmail.com>
2022-02-25 02:15:27 +0300

    edit icons in comp

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -34,47 +34,25 @@ import BaseIcon from './BaseIcon.vue'
     <!--right-->
     <div class="flex items-center justify-end lg:w-1/4 sm:space-x-3 p-2 sm:px-4">
       <button class="sm:hidden p-2 focus:outline-none">
-        <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          />
-        </svg>
+        <BaseIcon name="search" class="h-5 w-5" />
       </button>
 
       <button class="sm:hidden p-2 focus:outline-none">
-        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            clip-rule="evenodd"
-            d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
-            fill-rule="evenodd"
-          />
-        </svg>
+        <BaseIcon name="microphone" class="h-5 w-5" />
       </button>
 
       <!--pop-up btn list 1-->
       <button class="relative group p-2 focus:outline-none">
-        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-          />
-        </svg>
+        <BaseIcon name="viewGrid" class="h-5 w-5" />
+
+        <!--<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path></svg>-->
         <!--TODO: переделать-->
         <!--<TheDropdownApps />-->
       </button>
 
       <!--pop-up btn list 2-->
       <button class="relative group p-2 focus:outline-none">
-        <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          />
-        </svg>
+        <BaseIcon name="dotsVertical" class="h-5 w-5" />
         <!--TODO: переделать, второй список-->
         <!--<TheDropdownSettings />-->
       </button>
--- src/components/TheSearchButton.vue
+++ src/components/TheSearchButton.vue
@@ -1,16 +1,11 @@
-<script setup></script>
+<script setup>
+import BaseIcon from './BaseIcon.vue'
+</script>
 
 <template>
   <button
-    class='flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200 focus:outline-none'
+    class="flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200 focus:outline-none"
   >
-    <svg class='h-5 w-5' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-      <path
-        d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
-      />
-    </svg>
+    <BaseIcon name="search" class="h-5 w-5" />
   </button>
 </template>
