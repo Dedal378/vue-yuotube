43c53ce1c91b85bf6c6367297d8b53d17655e5fe
Dedal378 <orlovetsfoto@gmail.com>
2022-02-25 02:21:54 +0300

    edit icons in comp

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import BaseIcon from './BaseIcon.vue'
+</script>
 
 <template>
   <aside class="hidden xl:block w-64 max-h-screen fixed z-20 top-0 overflow-auto pt-12 bg-white">
@@ -76,13 +78,7 @@
       <button
         class="flex items-center px-2 py-0.5 mt-3 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm focus:outline-none"
       >
-        <svg class="h-7 w-7 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            clip-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-            fill-rule="evenodd"
-          />
-        </svg>
+        <BaseIcon name="user" class="h-7 w-7 mr-2" />
         Sign in
       </button>
     </section>
