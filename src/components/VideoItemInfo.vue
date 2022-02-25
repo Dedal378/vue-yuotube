87e7807153ce5189e5f063fe3f35826e595ea1e5
Dedal378 <orlovetsfoto@gmail.com>
2022-02-25 06:59:19 +0300

    add icons in VideoItem.vue

---
+++ src/components/VideoItemInfo.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue'
+import BaseIcon from './BaseIcon.vue'
+
+defineProps({
+  index: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex items-start mt-3">
+    <img alt="image" class="mr-3 rounded-full w-9 h-9" :src="`https://picsum.photos/seed/avatar${index}/68`" />
+
+    <div class="text-xs whitespace-nowrap">
+      <span class="font-semibold text-gray-800">Video title 1</span>
+
+      <div class="flex items-center mt-1">
+        <span class="mb-1">Channel name 1</span>
+        <BaseIcon name="check" class="h-3.5 w-3.5 ml-1" />
+      </div>
+
+      <div>
+        <span>1k views</span>
+        &middot;
+        <span>1 day ago</span>
+      </div>
+    </div>
+
+    <button
+      class="ml-auto p-0.5 -mt-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
+    >
+      <BaseIcon name="dotsVertical" class="h-5 w-5" />
+    </button>
+  </div>
+</template>
---
+++ src/components/VideoItemThumbnail.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue'
+import BaseIcon from './BaseIcon.vue'
+
+defineProps({
+  index: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="relative">
+    <img alt="image" :src="`https://picsum.photos/seed/image${index}/720/404`" />
+
+    <span
+      class="absolute opacity-0 group-hover:opacity-100 bg-opacity-60 top-0 right-0 bg-black text-white rounded-sm m-1 p-1"
+    >
+      <BaseIcon name="clock" class="h-5 w-5" />
+    </span>
+    <span
+      class="absolute opacity-0 group-hover:opacity-100 bg-opacity-60 top-8 right-0 bg-black text-white rounded-sm m-1 p-1"
+    >
+      <BaseIcon name="menuVideo" class="h-5 w-5" />
+    </span>
+    <span
+      class="absolute opacity-100 group-hover:opacity-0 duration-500 bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold"
+    >
+      6:35
+    </span>
+  </div>
+</template>
