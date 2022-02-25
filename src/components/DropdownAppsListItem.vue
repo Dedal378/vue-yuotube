cd389a174ea4e28a8e7b94c300f1d483c803b1c0
Dedal378 <orlovetsfoto@gmail.com>
2022-02-25 03:15:16 +0300

    add DropdownAppsListItem.vue

---
+++ src/components/DropdownAppsListItem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BaseIcon from './BaseIcon.vue'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <li>
+    <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
+      <BaseIcon name="play" class="h-6 w-6 mr-3 text-red-500" />
+      <span>{{ props.label }}</span>
+    </a>
+  </li>
+</template>
--- src/components/TheDropdownApps.vue
+++ src/components/TheDropdownApps.vue
@@ -1,104 +1,26 @@
-<script setup></script>
+<script setup>
+import DropdownAppsListItem from './DropdownAppsListItem.vue'
+</script>
 
 <template>
   <div class="opacity-0 group-hover:opacity-100 absolute right-0 sm:left-0 top-9 bg-white w-60 border border-t-0">
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
-            <svg
-              class="h-6 w-6 mr-3 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                fill-rule="evenodd"
-              />
-            </svg>
-            <span>YouTube TV</span>
-          </a>
-        </li>
+        <DropdownAppsListItem label="YouTube TV" />
       </ul>
     </section>
 
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
-            <svg
-              class="h-6 w-6 mr-3 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                fill-rule="evenodd"
-              />
-            </svg>
-            <span>YouTube Music</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
-            <svg
-              class="h-6 w-6 mr-3 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                fill-rule="evenodd"
-              />
-            </svg>
-            <span>YouTube Kids</span>
-          </a>
-        </li>
+        <DropdownAppsListItem label="YouTube Music" />
+        <DropdownAppsListItem label="YouTube Kids" />
       </ul>
     </section>
 
     <section class="py-2">
       <ul>
-        <li>
-          <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
-            <svg
-              class="h-6 w-6 mr-3 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                fill-rule="evenodd"
-              />
-            </svg>
-            <span>Creator Academy</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-3 py-2 text-sm hover:bg-gray-100" href="#">
-            <svg
-              class="h-6 w-6 mr-3 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                clip-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                fill-rule="evenodd"
-              />
-            </svg>
-            <span>YouTube for Artist</span>
-          </a>
-        </li>
+        <DropdownAppsListItem label="Creator Academy" />
+        <DropdownAppsListItem label="YouTube for Artist" />
       </ul>
     </section>
   </div>
