c8ee2ef9a634e990993af7a57639d97cae92dfa0
Dedal378 <orlovetsfoto@gmail.com>
2022-02-25 06:06:28 +0300

    edit TheDropdownSettings.vue

---
+++ src/components/DropdownSettingsListItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+import BaseIcon from './BaseIcon.vue'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  withSubMenu: {
+    type: Boolean,
+  },
+})
+</script>
+
+<template>
+  <li>
+    <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
+      <BaseIcon v-if="icon" :name="icon" class="h-6 w-6 mr-3 text-gray-400" />
+      <span>{{ props.label }}</span>
+      <BaseIcon v-if="withSubMenu" name="arrow" class="h-5 w-5 text-gray-400 ml-auto" />
+    </a>
+  </li>
+</template>
--- src/components/TheDropdownSettings.vue
+++ src/components/TheDropdownSettings.vue
@@ -1,74 +1,37 @@
 <script setup>
-import BaseIcon from './BaseIcon.vue'
+import { reactive } from 'vue'
+import DropdownSettingsListItem from './DropdownSettingsListItem.vue'
+
+const listItems = reactive([
+  { label: 'Appearance: Device theme', icon: 'theme', withSubMenu: true },
+  { label: 'Language: English', icon: 'language', withSubMenu: true },
+  { label: 'Location: United States', icon: 'location', withSubMenu: true },
+  { label: 'Settings', icon: 'settings', withSubMenu: false },
+  { label: 'Your data in YouTube', icon: 'data', withSubMenu: false },
+  { label: 'Help', icon: 'help', withSubMenu: false },
+  { label: 'Send feedback', icon: 'feedback', withSubMenu: false },
+  { label: 'Keyboard shortcuts', icon: 'shortcut', withSubMenu: false },
+  { label: 'Restricted mode: Off', icon: null, withSubMenu: true },
+])
 </script>
 
 <template>
   <div class="opacity-100 absolute top-9 -right-full sm:right-0 bg-white w-72 border border-t-0">
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="theme" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Appearance: Device theme</span>
-            <BaseIcon name="arrow" class="h-5 w-5 text-gray-400 ml-auto" />
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="language" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Language: English</span>
-            <BaseIcon name="arrow" class="h-5 w-5 text-gray-400 ml-auto" />
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="location" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Location: United States</span>
-            <BaseIcon name="arrow" class="h-5 w-5 text-gray-400 ml-auto" />
-          </a>
-        </li>
-
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="settings" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Settings</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="data" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Your data in YouTube</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="help" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Help</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="feedback" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Send feedback</span>
-          </a>
-        </li>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <BaseIcon name="shortcut" class="h-6 w-6 mr-3 text-gray-400" />
-            <span>Keyboard shortcuts</span>
-          </a>
-        </li>
+        <DropdownSettingsListItem
+          v-for="listItem in listItems.slice(0, 8)"
+          :key="listItem.label"
+          :label="listItem.label"
+          :icon="listItem.icon"
+          :with-sub-menu="listItem.withSubMenu"
+        />
       </ul>
     </section>
 
     <section class="py-2">
       <ul>
-        <li>
-          <a class="flex items-center px-4 py-2 text-sm hover:bg-gray-100" href="#">
-            <span>Restricted Mode: Off</span>
-            <BaseIcon name="arrow" class="h-5 w-5 text-gray-400 ml-auto" />
-          </a>
-        </li>
+        <DropdownSettingsListItem :label="listItems[8].label" :with-sub-menu="listItems[8].withSubMenu" />
       </ul>
     </section>
   </div>
