ad71691fa474e18c7fdbf5d8db504a8a65c0cbac
Dedal378 <orlovetsfoto@gmail.com>
2022-03-06 01:14:39 +0300

    refactor TheDropdownApps.vue: add close dropdown

--- src/components/TheDropdownApps.vue
+++ src/components/TheDropdownApps.vue
@@ -1,27 +1,49 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import DropdownAppsListItem from './DropdownAppsListItem.vue'
+import BaseIcon from './BaseIcon.vue'
+
+const isOpen = ref(false)
+const el = ref(null)
+
+const closeDropDown = () => (isOpen.value = !isOpen.value)
+
+// обработка click вне элемента
+onMounted(() => {
+  window.addEventListener('click', (ev) => {
+    if (!el.value.contains(ev.target)) {
+      closeDropDown()
+    }
+  })
+})
 </script>
 
 <template>
-  <div class="opacity-0 group-hover:opacity-100 absolute right-0 sm:left-0 top-9 bg-white w-60 border border-t-0">
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownAppsListItem label="YouTube TV" />
-      </ul>
-    </section>
+  <div class="relative">
+    <button @click="closeDropDown" ref="el" class="relative group p-2 focus:outline-none">
+      <BaseIcon name="viewGrid" class="h-5 w-5" />
+    </button>
 
-    <section class="py-2 border-b">
-      <ul>
-        <DropdownAppsListItem label="YouTube Music" />
-        <DropdownAppsListItem label="YouTube Kids" />
-      </ul>
-    </section>
+    <div v-show="isOpen" class="absolute right-0 sm:left-0 top-9 bg-white w-60 border border-t-0">
+      <section class="py-2 border-b">
+        <ul>
+          <DropdownAppsListItem label="YouTube TV" />
+        </ul>
+      </section>
 
-    <section class="py-2">
-      <ul>
-        <DropdownAppsListItem label="Creator Academy" />
-        <DropdownAppsListItem label="YouTube for Artist" />
-      </ul>
-    </section>
+      <section class="py-2 border-b">
+        <ul>
+          <DropdownAppsListItem label="YouTube Music" />
+          <DropdownAppsListItem label="YouTube Kids" />
+        </ul>
+      </section>
+
+      <section class="py-2">
+        <ul>
+          <DropdownAppsListItem label="Creator Academy" />
+          <DropdownAppsListItem label="YouTube for Artist" />
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-// import TheDropdownApps from './TheDropdownApps.vue'
+import TheDropdownApps from './TheDropdownApps.vue'
 // import TheDropdownSettings from './TheDropdownSettings.vue'
 import LogoMain from './LogoMain.vue'
 import TheSearch from './TheSearch.vue'
@@ -46,13 +46,7 @@ defineEmits({
       </button>
 
       <!--pop-up btn list 1-->
-      <button class="relative group p-2 focus:outline-none">
-        <BaseIcon name="viewGrid" class="h-5 w-5" />
-
-        <!--<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path></svg>-->
-        <!--TODO: переделать-->
-        <!--<TheDropdownApps />-->
-      </button>
+      <TheDropdownApps />
 
       <!--pop-up btn list 2-->
       <button class="relative group p-2 focus:outline-none">
