e99096d3adcc3da576f3c7f98b4c14a8a308cb1b
Dedal378 <orlovetsfoto@gmail.com>
2022-03-06 05:48:17 +0300

    refactor VideoItemThumbnail.vue split comp-те, add animate

--- src/components/VideoItemThumbnail.vue
+++ src/components/VideoItemThumbnail.vue
@@ -1,5 +1,5 @@
 <script setup>
-import BaseIcon from './BaseIcon.vue'
+import VideoItemThumbnailButton from './VideoItemThumbnailButton.vue'
 
 defineProps({
   index: {
@@ -13,16 +13,9 @@ defineProps({
   <div class="relative">
     <img alt="image" :src="`https://picsum.photos/seed/image${index}/720/404`" />
 
-    <span
-      class="absolute opacity-0 group-hover:opacity-100 bg-opacity-60 top-0 right-0 bg-black text-white rounded-sm m-1 p-1"
-    >
-      <BaseIcon name="clock" class="h-5 w-5" />
-    </span>
-    <span
-      class="absolute opacity-0 group-hover:opacity-100 bg-opacity-60 top-8 right-0 bg-black text-white rounded-sm m-1 p-1"
-    >
-      <BaseIcon name="menuVideo" class="h-5 w-5" />
-    </span>
+    <VideoItemThumbnailButton icon="clock" label="Watch later" classTop="top-0" />
+    <VideoItemThumbnailButton icon="menuVideo" label="Add to queue" classTop="top-8" />
+
     <span
       class="absolute opacity-100 group-hover:opacity-0 duration-500 bottom-0 right-0 bg-black text-white rounded-sm m-1 p-1 text-xs font-semibold"
     >
