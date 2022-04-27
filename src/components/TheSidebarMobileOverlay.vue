43bd41e9d018a7053f831e0ce8a914980d6c5403
Aleksandr Orlovets <orlovetsjob@gmail.com>
2022-04-27 19:21:04 +0300

    adding dist

--- src/components/TheSidebarMobileOverlay.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="fixed inset-0 bg-black bg-opacity-50 z-30"></div>
-</template>
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import WindiCSS from 'vite-plugin-windicss'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), WindiCSS()],
-  base: '/vue-yuotube/',
-})
